9c7caf7734d2363768947103ab16e70b67808c43
marwanhaz43 <marwanhaz43@gmail.com>
2021-09-14 15:41:46 +0100

    deploy2

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,10 +1,10 @@
-export const CommonNavbar = () => import('../..\\components\\CommonNavbar.vue' /* webpackChunkName: "components/common-navbar" */).then(c => wrapFunctional(c.default || c))
-export const CartProductsList = () => import('../..\\components\\cart\\CartProductsList.vue' /* webpackChunkName: "components/cart-products-list" */).then(c => wrapFunctional(c.default || c))
-export const ProductsListCommonProductsListPage = () => import('../..\\components\\products-list\\CommonProductsListPage.vue' /* webpackChunkName: "components/products-list-common-products-list-page" */).then(c => wrapFunctional(c.default || c))
-export const ProductsListProductListCard = () => import('../..\\components\\products-list\\ProductListCard.vue' /* webpackChunkName: "components/products-list-product-list-card" */).then(c => wrapFunctional(c.default || c))
-export const ProductsList = () => import('../..\\components\\products-list\\ProductsList.vue' /* webpackChunkName: "components/products-list" */).then(c => wrapFunctional(c.default || c))
-export const ProductsListFilter = () => import('../..\\components\\products-list\\ProductsListFilter.vue' /* webpackChunkName: "components/products-list-filter" */).then(c => wrapFunctional(c.default || c))
-export const ProductGallery = () => import('../..\\components\\product-details\\ProductGallery.vue' /* webpackChunkName: "components/product-gallery" */).then(c => wrapFunctional(c.default || c))
+export { default as CommonNavbar } from '../..\\components\\CommonNavbar.vue'
+export { default as CartProductsList } from '../..\\components\\cart\\CartProductsList.vue'
+export { default as ProductGallery } from '../..\\components\\product-details\\ProductGallery.vue'
+export { default as ProductsListCommonProductsListPage } from '../..\\components\\products-list\\CommonProductsListPage.vue'
+export { default as ProductsListProductListCard } from '../..\\components\\products-list\\ProductListCard.vue'
+export { default as ProductsList } from '../..\\components\\products-list\\ProductsList.vue'
+export { default as ProductsListFilter } from '../..\\components\\products-list\\ProductsListFilter.vue'
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
--- .nuxt/dist/client/e97a4ef.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{252:function(n,e,d){"use strict";d.r(e);var l=d(29),component=Object(l.a)({},undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
